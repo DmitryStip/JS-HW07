086ada304d36bee1876e9cd3d3b36911d845ce32
Dmitry <rekssobaka@gmail.com>
2023-05-30 21:11:17 +0300

    rework

--- src/script.js
+++ src/script.js
@@ -6,32 +6,33 @@
 //  @returns {number} -  рейтинг победителя
  
 function calculateRate(winnerRate, opponentRate) {
-    // Проверяем ввод
-    if (
-      typeof winnerRate !== 'number' ||
-      typeof opponentRate !== 'number' ||
-      winnerRate < 0 ||
-      opponentRate < 0
-    ) {
-      console.log('Inccorect rating.');
-    }
-  
-    // Разница
-    const ratingDiff = opponentRate - winnerRate;
-  
-    // При победе над более слабым или равным по рейтингу игроком
-    if (ratingDiff >= 0 && ratingDiff <= 2) {
-      return winnerRate + 2;
-    } else if (ratingDiff > 2 && ratingDiff < 20) {
-      return winnerRate + 1;
-    } else if (ratingDiff >= 20) {
-      return winnerRate;
-    }
-  
-    // При победе над более сильным
-    return Math.round((opponentRate - winnerRate + 5) / 3);
+  if (
+    typeof winnerRate !== 'number' ||
+    typeof opponentRate !== 'number' ||
+    winnerRate < 0 ||
+    opponentRate < 0
+  ) {
+    return 'Incorrect rating.';
   }
-  const winnerRate = 10;
+
+  const ratingDiff = opponentRate - winnerRate;
+
+  if (ratingDiff >= 0 && ratingDiff <= 2) {
+    return winnerRate + 2;
+  }
+
+  if (ratingDiff > 2 && ratingDiff < 20) {
+    return winnerRate + 1;
+  }
+
+  if (ratingDiff >= 20) {
+    return 0;
+  }
+
+  return Math.round((opponentRate - winnerRate + 5) / 3);
+}
+
+const winnerRate = 10;
 const opponentRate = 15;
 
 const newRate = calculateRate(winnerRate, opponentRate);
