e9ff72b4f160103cde4da3678d83374fc4926513
Dmitry <rekssobaka@gmail.com>
2023-07-20 14:18:05 +0300

    rework

--- src/animals.js
+++ src/animals.js
@@ -1,107 +1,184 @@
 'use strict';
 
 class Animals {
-  constructor(age, name, weight, limbAmount) {
-    this.age = age;
-    this.name = name;
-    this.weight = weight;
-    this.limbAmount = limbAmount;
+  constructor(age, name, weight, limbAmount){
+      this.age = age;
+      this.name = name;
+      this.weight = weight;
+      this.limbAmount = limbAmount;
   }
+ move(){
+  return `Animals moving`
+ }
+ say(){
+  return `Animals say`
+ }
+ eat(){
+  return `Animals are eating`
+ }
+};
 
-  move() {
-    console.log(`${this.name} двигается.`);
+class Mammals extends Animals{};
+class Predators extends Mammals{};
+class Whales extends Mammals{};
+class Primates extends Mammals{};
+
+class Birds extends Animals{
+  static maxAge = 15;
+  static maxWeight = 30;
+
+constructor(age, name, weight, limbAmount){
+      super(age, name, weight, limbAmount);
   }
+  move(){
+      return `Birds flying`
+         }
+  say(){
+      return `Birds chirping `
+         }
+  eat(){
+      return `Birds are eating`
+         }
+  static isBirds(obj) {
+      return obj instanceof Birds;
+  } 
+};
 
-  say() {
-    console.log(`${this.name} издает звук.`);
+class Fish extends Animals{
+  static maxAge = 15;
+  static maxWeight = 100;
+
+constructor(age, name, weight, limbAmount){
+      super(age, name, weight, limbAmount);
   }
+  move(){
+      return `Fishes swim`
+         }
+  say(){
+      return `Fishes dont speak`
+         }
+  eat(){
+      return `Fishes are eating`
+         }
+  static isFish(obj) {
+      return obj instanceof Fish;
+      }
+};
 
-  eat() {
-    console.log(`${this.name} ест.`);
+class Dog extends Predators{
+  static maxAge = 18;
+  static maxWeight = 150;
+
+constructor(age, name, weight, limbAmount){
+      super(age, name, weight, limbAmount);
   }
-}
+  move(){
+      return `Dogs run`
+         }
+  say(){
+      return `Dogs barking`
+         }
+  eat(){
+      return `Dogs are eating`
+         }
 
-class Mammals extends Animals {}
-
-class Birds extends Animals {}
-
-class Fish extends Animals {}
-
-class Predators extends Mammals {
-  static isPredators(obj) {
-    return obj instanceof Predators;
-  }
-}
-
-class Whales extends Mammals {
-  static isWhales(obj) {
-    return obj instanceof Whales;
-  }
-}
-
-class Primates extends Mammals {
-  static isPrimates(obj) {
-    return obj instanceof Primates;
-  }
-}
-
-class Dog extends Predators {
   static isDog(obj) {
-    return obj instanceof Dog;
-  }
+      return obj instanceof Dog;
+      };
 }
 
-class Dolphin extends Whales {
+class Dolphin extends Whales{
+  static maxAge = 20;
+  static maxWeight = 120;
+
+constructor(age, name, weight, limbAmount){
+      super(age, name, weight, limbAmount);
+  }
+  move(){
+      return `Dolphins swim`
+          }
+  say(){
+      return `Dolphins dont speak`
+          }
+  eat(){
+      return `Dolphins are eating`
+          }
+
   static isDolphin(obj) {
-    return obj instanceof Dolphin;
-  }
+      return obj instanceof Dolphin;
+  };
 }
+class Human extends Primates{
+  static maxAge = 120;
+  static maxWeight = 300;
 
-class Human extends Primates {
+constructor(age, name, weight, limbAmount){
+      super(age, name, weight, limbAmount);
+  }
+  move(){
+      return `People walking`
+          }
+  say(){
+      return `People talk`
+          }
+  eat(){
+      return `People are eating`
+          }
+
   static isHuman(obj) {
-    return obj instanceof Human;
-  }
+      return obj instanceof Human;
+  };
 }
 
-Animals.maxAge = 100;
-Animals.maxWeight = 1000;
-Birds.maxAge = 20;
-Birds.maxWeight = 10;
-Fish.maxAge = 10;
-Fish.maxWeight = 5;
+const lizard = new Animals(1, 'lizard', '200g', 4);
+console.log(lizard);
+console.log(lizard.move());
+console.log(lizard.say());
+console.log(lizard.eat());
 
-const mammal1 = new Mammals(5, "Mammal 1", 50, 4);
-const mammal2 = new Mammals(3, "Mammal 2", 30, 4);
-const bird1 = new Birds(1, "Bird 1", 0.5, 2);
-const bird2 = new Birds(2, "Bird 2", 0.3, 2);
-const fish1 = new Fish(2, "Fish 1", 0.2, 0);
-const fish2 = new Fish(1, "Fish 2", 0.1, 0);
-const predator1 = new Predators(8, "Predator 1", 100, 4);
-const predator2 = new Predators(6, "Predator 2", 80, 4);
-const whale1 = new Whales(20, "Whale 1", 5000, 0);
-const whale2 = new Whales(15, "Whale 2", 4000, 0);
-const primate1 = new Primates(30, "Primate 1", 70, 2);
-const primate2 = new Primates(25, "Primate 2", 60, 2);
-const dog1 = new Dog(4, "Dog 1", 20, 4);
-const dog2 = new Dog(3, "Dog 2", 15, 4);
-const dolphin1 = new Dolphin(12, "Dolphin 1", 2000, 0);
-const dolphin2 = new Dolphin(10, "Dolphin 2", 1800, 0);
-const human1 = new Human(40, "Human 1", 70, 2);
-const human2 = new Human(35, "Human 2", 65, 2);
+const fox = new Animals(4, 'fox', '8 kg', 4);
+console.log(fox);
+console.log(fox.move());
+console.log(fox.say());
+console.log(fox.eat());
 
-mammal1.move();
-mammal2.say();
-console.log(`Возраст: ${bird1.age}, Вес: ${bird1.weight}`);
-bird2.eat();
-console.log(`Количество конечностей: ${fish1.limbAmount}`);
-fish2.move();
-predator1.say();
-console.log(`Это собака? ${Dog.isDog(predator2)}`);
-whale1.eat();
-console.log(`Это дельфин? ${Dolphin.isDolphin(whale2)}`);
-primate1.move();
-primate2.say();
-console.log(`Это человек? ${Human.isHuman(primate1)}`);
-dog1.eat();
-dolphin2.move();
-human1.say();
+const sparrow = new Birds(2, 'sparrow', '40g', 4)
+console.log(sparrow);
+console.log(sparrow.move());
+console.log(sparrow.say());
+console.log(sparrow.eat());
+
+const eagle = new Birds(10, 'Eagle', '3kg', 4)
+console.log(eagle);
+console.log(eagle.move());
+console.log(eagle.say());
+console.log(eagle.eat());
+
+const pike = new Fish(10, 'pike', '3kg', 3)
+console.log(pike);
+console.log(pike.move());
+console.log(pike.say());
+console.log(pike.eat());
+
+const terrier= new Dog(4, 'terrier', '3.2kg', 4)
+console.log(terrier);
+console.log(terrier.move());
+console.log(terrier.say());
+console.log(terrier.eat());
+
+const dolphin = new Dolphin(10, 'Dolphin', '100kg', 3)
+console.log(dolphin);
+console.log(dolphin.move());
+console.log(dolphin.say());
+console.log(dolphin.eat());
+
+const biden = new Human(80, 'Biden', '70kg', 4)
+console.log(biden);
+console.log(biden.move());
+console.log(biden.say());
+console.log(biden.eat());
+
+console.log(Human.isHuman(sparrow));
+console.log(Human.isHuman(biden));
+console.log(Dolphin.isDolphin(lizard));
+
